c156834c72d80d33ab57ec48a2c7dc3b77b4ef68
Tuan Le <letuanlttt@gmail.com>
2023-03-30 01:44:21 +0700

    fix: should return process result to zalo callback

--- APIGateway/Controllers/DonationController.cs
+++ APIGateway/Controllers/DonationController.cs
@@ -32,57 +32,10 @@ public class DonationController : ControllerBase
 
     [HttpPost]
     [Route("zalo-callBack")]
-    public async Task<IActionResult> CallBack(ZaloCallbackCommand request)
+    public async Task<IActionResult> ZaloPayCallBack(ZaloCallbackCommand request)
     {
-        // var result = new Dictionary<string, object>();
-        // string key2 = "trMrHtvjo6myautxDUiAcYsVtaeQ8nhf";
-        //
-        // try {
-        //     var dataStr = Convert.ToString(cbdata.data);
-        //     var reqMac = Convert.ToString(cbdata.mac);
-        //
-        //     var mac = HmacHelper.Compute(ZaloPayHMAC.HMACSHA256, key2, dataStr);
-        //
-        //     Console.WriteLine("mac = {0}", mac);
-        //
-        //     // kiểm tra callback hợp lệ (đến từ ZaloPay server)
-        //     if (!reqMac.Equals(mac)) {
-        //         // callback không hợp lệ
-        //         result["return_code"] = -1;
-        //         result["return_message"] = "mac not equal";
-        //     }
-        //     else {
-        //         // thanh toán thành công
-        //         // merchant cập nhật trạng thái cho đơn hàng
-        //         var dataJson = JsonConvert.DeserializeObject<Dictionary<string, object>>(dataStr);
-        //         Console.WriteLine("update order's status = success where app_trans_id = {0}", dataJson["app_trans_id"]);
-        //
-        //         result["return_code"] = 1;
-        //         result["return_message"] = "success";
-        //     }
-        // } catch (Exception ex) {
-        //     result["return_code"] = 0; // ZaloPay server sẽ callback lại (tối đa 3 lần)
-        //     result["return_message"] = ex.Message;
-        // }
-
-        // thông báo kết quả cho ZaloPay server
-        // return Ok(result); 
-
-        await _mediator.Send(request);
-        throw new NotImplementedException();
-
+        var processResult =await _mediator.Send(request);
+        return Ok(processResult);
     }
     
-    public class BankDTO {
-        public string bankcode { get; set; }
-        public string name { get; set; }
-        public int displayorder { get; set; }
-        public int pmcid { get; set; }
-    }
-
-    class BankListResponse {
-        public string returncode { get; set; }
-        public string returnmessage { get; set; }
-        public Dictionary<string, List<BankDTO>> banks { get; set; }
-    }
 }
